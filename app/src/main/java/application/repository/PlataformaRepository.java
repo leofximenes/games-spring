37dcbe0d9f72f92122f060ec02706c7f348eb599
leofximenes <lximenes63@outlook.com>
2024-10-11 01:31:30 +0000

    new file:   app/src/main/java/application/controller/CategoriaController.java 	new file:   app/src/main/java/application/controller/JogoController.java 	new file:   app/src/main/java/application/controller/PlataformaController.java 	new file:   app/src/main/java/application/model/Categoria.java 	new file:   app/src/main/java/application/model/Jogo.java 	new file:   app/src/main/java/application/model/Plataforma.java 	new file:   app/src/main/java/application/repository/CategoriaRepository.java 	new file:   app/src/main/java/application/repository/JogoRepository.java 	new file:   app/src/main/java/application/repository/PlataformaRepository.java 	renamed:    app/src/main/java/application/WebApplication.java -> app/src/main/resources/resources/WebApplication.java 	renamed:    app/src/main/resources/application.properties -> app/src/main/resources/resources/application.properties 	new file:   app/src/main/webapp/WEB-INF/categoria/delete.jsp 	new file:   app/src/main/webapp/WEB-INF/categoria/insert.jsp 	new file:   app/src/main/webapp/WEB-INF/categoria/list.jsp 	new file:   app/src/main/webapp/WEB-INF/categoria/update.jsp

---
+++ app/src/main/java/application/repository/PlataformaRepository.java
@@ -0,0 +1,9 @@
+package application.repository;
+
+import org.springFramework.data.repository.CrudRepository;
+
+import application.Plataforma.Categoria;
+
+public interface PlataformaRepository extends CrudRepository<Plataforma>, Long > {
+    
+}
